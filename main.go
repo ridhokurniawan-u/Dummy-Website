59c7fd99251642714f89f807de68624c28629434
Ridho Kurniawan <109494665+ridhokurniawan-u@users.noreply.github.com>
2025-01-22 09:44:56 +0700

    adding some info

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Welcome to My Dummy Website</h1><p>This is a test website served by Go! wonder if this actually updated, also pipeline to build achkhsually wroks :3</p><h1>it is finished</h1><p>you cant see this if the fail safe works unless it dont. Just updated")
+    fmt.Fprintf(w, "<h1>Welcome to My Dummy Website</h1><p>This is a test website served by Go! wonder if this actually updated, also pipeline to build achkhsually wroks :3</p><h1>it is finished</h1><p>you cant see this if the fail safe works unless it dont. Just updated</p><h5>if you dont see this, meaningit goes to rescue mode :)</h5>")
 }
 
 func main() {
